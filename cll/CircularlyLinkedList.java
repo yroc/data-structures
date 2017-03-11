4f041f5f12cea39115356a665e810d4e4ca4f03a
cory <yrocsand@gmail.com>
2017-03-11 13:03:49 -0500

    Add methods

--- cll/CircularlyLinkedList.java
+++ cll/CircularlyLinkedList.java
@@ -6,5 +6,64 @@ public class CircularlyLinkedList<E>
         // Attributes
         private E element;
         private Node<E> next;
+
+        // Constructor
+        public Node(E element, Node<E> next)
+        {
+            this.element = element;
+            this.next = next;
+        }
+
+        // Methods
+        public E getElement()
+        {
+            return this.element;
+        }
+
+        public Node<E> getNext()
+        {
+            return this.next;
+        }
+
+        public void setNext(Node<E> next)
+        {
+            this.next = next;
+        }
+    }
+    // ------------ end of nested Node class ------------
+
+    // Instance variables (of CLL)
+    private Node<E> tail;
+    private int size;
+
+    // Constructor
+    public CircularlyLinkedList()
+    {
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // Access methods
+    
+    /** Returns the size of this CLL */
+    public int size()
+    {
+        return this.size;
+    }
+
+    /** Returns true if this CLL is empty, and false otherwise */
+    public boolean isEmpty()
+    {
+        return this.size == 0;
+    }
+
+    /** Returns (but does not remove) the first element in this CLL */
+    public E first()
+    {
+        if (this.isEmpty())
+            {
+                return null;
+            }
+        return this.tail.getNext().getElement();
     }
 }
