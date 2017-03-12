733c633a1c02a650e9b54c1308588fec38c23548
cory <yrocsand@gmail.com>
2017-03-12 16:09:26 -0400

    Rewrite insertionSort() method in way that makes more sense to me

--- insertion_sort/InsertionSort.java
+++ insertion_sort/InsertionSort.java
@@ -2,21 +2,19 @@ public class InsertionSort
 {
     public static void insertionSort(char[] data)
     {
-        int n = data.length;
-        for (int k = 1; k < n; k++) // k is the index of current element,
-                                    // beginning with the first element (not the
-                                    // zeroth)
+        for (int i = 1; i < data.length; i++)
             {
-                char cur = data[k]; // cur is the current element
-                int j = k;          // j is the eventual position of the current
-                                    // character, initialized to the current
-                                    // character's current position
-                while (j > 0 && data[j - 1] > cur)
+                int cur = i;
+                int pre = i - 1;
+
+                while (cur > 0 && data[cur] < data[pre])
                     {
-                        data[j] = data[j - 1];
-                        j--;
+                        char temp = data[pre];
+                        data[pre] = data[cur];
+                        data[cur] = temp;
+                        cur--;
+                        pre--;
                     }
-                data[j] = cur;
             }
     }
 }
