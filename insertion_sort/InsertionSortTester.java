98e5621ebe0f555dd9a076d3eccb72f13de21567
cory <yrocsand@gmail.com>
2017-03-12 18:20:49 -0400

    Add Javadoc comments

--- insertion_sort/InsertionSortTester.java
+++ insertion_sort/InsertionSortTester.java
@@ -1,5 +1,14 @@
+/**
+ * A class to test the InsertionSort class.
+ */
+
 public class InsertionSortTester
 {
+    /**
+     * Creates a character array and passes it to InsertionSort.insertionSort to
+     * see if the array gets successfully sorted.
+     *
+     */
     public static void main(String[] args)
     {
         char[] a = {'C', 'E', 'B', 'D', 'A', 'I', 'J', 'L', 'K', 'H', 'G', 'F'};
