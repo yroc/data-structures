dbfca4afbb4e93b7057038737c26c7f71e1c7c90
cory <yrocsand@gmail.com>
2017-03-17 11:36:08 -0400

    Add TextAnalyzer.java

---
+++ text_analyzer/TextAnalyzer.java
@@ -0,0 +1,15 @@
+/**
+ * Tester class for the Text Analyzer application
+ */
+
+public class TextAnalyzer
+{
+    public static void main(String[] args)
+    {
+        BinarySearchTree BST = new BinarySearchTree();
+        BST.readIn("WisdomForRoad.txt");
+        System.out.println(BST.maxSearchPath());
+        BST.printWordsSorted();
+        BST.printTenMostCommonWords();
+    }
+}
