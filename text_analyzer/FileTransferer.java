cfd4ec8324ebbd3e4bfd819e98a06153c00485aa
cory <yrocsand@gmail.com>
2017-03-17 12:34:52 -0400

    Put file words into an (ordinary) array instead of a linked list

--- text_analyzer/FileTransferer.java
+++ text_analyzer/FileTransferer.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 
@@ -5,25 +6,28 @@ public class FileTransferer
 {
     public static void main(String[] args) throws Exception
     {
-        // Create a new String array to hold the words in the file
-        String[] filewords;
-        int numWords = 0;
+        String[] filewords;     // String array to hold the words in the given
+                                // file
+        int numWords = 0;       // The number of words in the file
 
-        // Scanner to read the words in the file
+        // Scanner to read the words in the file (for counting purposes)
         Scanner sc = new Scanner(new File("WisdomForRoad.txt"));
-        /*
+
         // Count the number of words in the file
         while(sc.hasNext())
             {
+                sc.next();
                 numWords++;
             }
-        System.out.println(numWords);
 
-
-        // Instantiate the array
+        // Instantiate the array to hold all the words
         filewords = new String[numWords];
+
+        // New Scanner to reread the words in the file (to put them in the
+        // array)
+        sc = new Scanner(new File("WisdomForRoad.txt"));
         
-        // Put the words into the linked list
+        // Put the words into the array
         for (int i = 0; i < numWords; i++)
             {
                 String s = sc.next();
@@ -37,7 +41,13 @@ public class FileTransferer
                 filewords[i] = s.toLowerCase();
             }
 
-        // Print the linked list (just to verify the code works)
-        System.out.println(filewords.toString());*/
+        // Print the array (just to verify the code works)
+        System.out.println(Arrays.toString(filewords));
+
+        // Sort the array
+        Arrays.sort(filewords);
+
+        // Print the sorted array (just to verify the code works)
+        System.out.println("\n" + Arrays.toString(filewords));
     }
 }
