f1a5a04b209fa4a9f9ee52788b60fdc09b4591c1
cory <yrocsand@gmail.com>
2017-03-17 11:35:37 -0400

    Add BinarySearchTree.java

---
+++ text_analyzer/BinarySearchTree.java
@@ -0,0 +1,47 @@
+/**
+ * A class to store all distinct words from a given text file
+ */
+
+public class BinarySearchTree extends LinkedBinaryTree
+{
+    /**
+     * Performs the following operations:
+     *
+     * 1) Reads from &lt;fileName&gt; word by word
+     *
+     * 2) For each new word found in &lt;fileName&gt;, a new BTNode is created
+     * and added to T according to the rules of BST insertion
+     *
+     * 3) For each duplicate word, the wordCounter of the corresponding
+     * BTNode is incremented
+     */
+    public void readIn(String fileName)
+    {
+        
+    }
+
+    /**
+     * Finds and returns the length of the longest search path in T
+     */
+    public int maxSearchPath()
+    {
+        
+    }
+
+    /**
+     * Prints all the distinct words found in &lt;fileName&gt;, in sorted order
+     */
+    public void printWordsSorted()
+    {
+        
+    }
+
+    /**
+     * Finds and prints the ten most common words from &lt;fileName&gt;,
+     * together with their respective wordCounter-s
+     */
+    public void prentTenMostCommonWords()
+    {
+        
+    }
+}
